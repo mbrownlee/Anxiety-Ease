774f7dcd27c2ac9af61cb4077de1411373b66a84
mbrownlee <michelle@orbwave.com>
2020-09-09 16:13:27 -0500

    fixing auth token issue

---
+++ anxietyapp/views/__init__.py
@@ -0,0 +1,3 @@
+from .user import UserView, UserSerializer
+from .register import register_user, login_user
+from .activity_type import ActivityTypeView
--- anxietyproject/urls.py
+++ anxietyproject/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from anxietyapp.models import *
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from anxietyapp.views import register_user, login_user
+from anxietyapp.views import ActivityTypeView, UserView
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'activitytype', ActivityTypeView, 'activitytype')
+router.register(r'users', UserView, 'user')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('register/', register_user),
+    path('login/', login_user),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
