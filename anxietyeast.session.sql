fdf7ae8e7fc902c2da7a232090be8bfabbf98f05
mbrownlee <michelle@orbwave.com>
2020-09-24 13:34:22 -0500

    adding technique

--- anxietyeast.session.sql
+++ anxietyeast.session.sql
@@ -11,3 +11,23 @@ insert into anxietyapp_staticactivityresource (resource, activity_type_id) value
 insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("Name 1 thing you can taste", 2);
 
 insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("You will not 'go crazy' because of this panic attack. This will NOT break you", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("Your anxiety is lying to you. You are loved. And you will be okay.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("This panic won't last. It will fade. You are taking steps to be okay.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("You are not going to lose control. You are taking control right now by thinking these positive thoughts.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("Trust your body to breath for you. Let go and let it take over. It will keep breahting.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("There is no real outside threat or attacker right now. Panic attacks are not dangerous.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("No one has ever died from a panic attack.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("You have survived every single panic attack you've ever had. You are strong.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("Your body will keep you safe.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("Panic attacks are terrifying but not dangerous. They are just a response to the heightened sensations we feel.", 3);
+
+insert into anxietyapp_staticactivityresource (resource, activity_type_id) values ("You and your body are capable of handling everything you are feeling right now.", 3);
